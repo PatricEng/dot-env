1596b4cde3cc22ac6c1554b5383aa504e711a99c
Patric Oliveira <patric.eng.computation@hotmail.com>
2021-10-16 20:20:04 -0300

    Update Environment.php

--- src/Environment.php
+++ src/Environment.php
@@ -5,7 +5,7 @@ namespace Patric\DotEnv;
 class Environment{
 
   /**
-   * Método responsável por carregar as variáveis de ambiente do projeto
+   * Método responsável por carregar as variáveis de ambiente do projeto 
    * @param  string $dir Caminho absoluto da pasta onde encontra-se o arquivo .env
    */
   public static function load($dir){
